cf99241a562ea359f65f2f788857d303c11e961c
Mikhail Mitiushin <dima.demyanov.1997@gmail.com>
2024-02-23 23:20:49 +0200

    Adding file structure

---
+++ src/features/items/itemsSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Item } from '../../types/itemTypes';
+
+
+interface ItemsState {
+  items: Item[];
+}
+
+const initialState: ItemsState = {
+  items: [],
+};
+
+export const itemsSlice = createSlice({
+  name: 'items',
+  initialState,
+  reducers: {
+    setItems: (state, action: PayloadAction<Item[]>) => {
+      state.items = action.payload;
+    },
+  },
+});
+
+export const { setItems } = itemsSlice.actions;
+
+export default itemsSlice.reducer;
